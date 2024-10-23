4b4881b10ed2e49d716f53aa2be784fafcc65dda
dilraj_raj_patel <108468806+Dilrajpp@users.noreply.github.com>
2024-10-23 12:30:23 +0000

    Scope level and mini hoisting in JavaScript

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -10,7 +10,7 @@ if (true) {
     let VarA = 10
     const VarB = 20
     var VarC = 30
-    console.log("INNER: ", VarA);
+    // console.log("INNER: ", VarA);
     
 }
 
@@ -20,8 +20,63 @@ if (true) {
 // }
 
 
-console.log(VarA);
+// console.log(VarA);
 // console.log(VarB);
 // console.log(VarC);
 
 
+//          ********************nested*****************
+
+
+function One(){
+    const username = "dilraj"
+
+    function Two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    Two()
+}
+
+// One()
+
+if (true){
+    const username = "dilraj"
+    if (username === "dilraj") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+
+
+//          ********************* intersting *****************************
+
+
+console.log(addOne(5))
+
+
+
+function addOne(num){
+    return num + 1
+}
+
+// addOne(5)
+
+
+
+addTwo(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+
+// addTwo(5)
