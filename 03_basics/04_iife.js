887b293238123f6d52381771befc7a87687d0037
dilraj_raj_patel <108468806+Dilrajpp@users.noreply.github.com>
2024-10-23 18:14:49 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -18,3 +18,40 @@
         console.log(`DB CONNECTED TWO ${name}`);
         
 })('dilraj')
+
+
+
+                //  ************NEXT***************
+
+                                            // JavaScript Execution Context
+
+                                            //Global EC  ---this keywords
+
+                                            // Global EC
+                                            // Function EC
+                                            // Eval EC
+
+
+                                            //This is two phase run
+                                            // 1. Memory Cretion Phase
+                                            // 2. Execution Phase
+
+
+            // How to Execute code
+
+            // 1. Global Execution ---this
+
+            // 2. Memory Phase --valu1-----> undefined
+            //                 --valu2-----> undefined
+            //                 --adnum------>definition
+            //                 --result1---->undefined
+            //                 --result2---->undefined
+
+            // 3. Execution Phase --val1-->10
+            //                    --val2-->5
+            //                    --addnum--->new variable environment + Execution thread    // eork end than end
+
+                                        // Memory Phase                  // Execution Context
+            //                           --valu1-----> undefined         // num1 --> 10
+            //                           --valu2-----> undefined         // num2 --> 5
+            //                           --total-----> undefined         // total --> 15
