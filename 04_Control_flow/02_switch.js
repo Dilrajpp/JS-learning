81ecde66601d85055d4408781abce17fc0d39e8a
dilraj_raj_patel <108468806+Dilrajpp@users.noreply.github.com>
2024-10-25 11:52:31 +0000

    Control Flow with Switch Operator & Truthy and False values

---
+++ 04_Control_flow/02_switch.js
@@ -0,0 +1,69 @@
+//  node 04_Control_flow/02_switch.js
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("fab");
+        break;
+
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        // break;
+        break;
+    case 5:
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
+
+
+
+const month2 = ("april")
+
+switch (month2) {
+    case "January":
+        console.log("January");
+        break;
+    case "fab":
+        console.log("fab");
+        break;
+
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        // break;
+        break;
+    case "may":
+        console.log("may");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
