0cd23b44092218982c5d41650fb1307ec5690bb5
dilraj_raj_patel <108468806+Dilrajpp@users.noreply.github.com>
2024-10-25 07:42:19 +0000

    control flow

---
+++ 04_Control_flow/01_controlflow.js
@@ -0,0 +1,97 @@
+//  node 04_Control_flow/01_controlflow.js
+
+// if 
+
+const isUserLoggedIn = true
+
+const temperature = 51
+
+// if ( temperature < 60 ){
+//     console.log("less than 60");
+    
+// }
+
+
+
+
+// if ( temperature === 51 ){
+//     console.log("less than 60");
+    
+// } else {
+
+// console.log("temperature is greater than 60");
+// }
+
+// if (isUserLoggedIn){
+
+// }
+
+
+
+
+// if ( 2 == "2"){
+//     console.log("executed");
+    
+// }
+
+
+// if ( 2 === "2"){
+//     console.log("executed");
+    
+// }
+
+// if ( 2 != 3){
+//     console.log("executed");
+    
+// }
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+
+
+
+// const score = 200 
+
+// if (score > 100) {
+//     // const power = "fly"
+//     // var power = "fly"
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+    
+// }
+// console.log(`User power: ${power}`);
+
+
+
+
+
+
+// const balance = 1000
+
+// // if (balance > 500) console.log("test"),console.log("test2");     // don't use this type code
+
+// // if (balance > 500) console.log("test"),  // use this type code
+// // console.log("test2");
+
+
+// if  (balance < 500) {
+//     console.log("less than");
+    
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 900");
+    
+// } else{
+//     console.log("less than 1200");
+    
+// }
+
+
+
+
+
+
+const userLoggedIn = t
