b030714fa345ccec6f18ee3d40409af6d3bebbbc
dilraj_raj_patel <108468806+Dilrajpp@users.noreply.github.com>
2024-11-27 12:02:40 +0000

    OOP

--- 15_classes_and_oop/oop.js
+++ 15_classes_and_oop/oop.js
@@ -6,14 +6,61 @@ const user = {
     getUserDetails: function(){
         // console.log("Got user details from database");
         // console.log(`Username: ${this.username}`);
-        console.log(this);
+        // console.log(this);
         
         
         
     }
 }
 
-console.log(user.username);
-console.log(user.getUserDetails);
+
+// const user2 = {
+//     username: "dilraj",
+//     loginCount: 8,
+//     signedIn: true,
+
+//     getUserDetails: function(){
+//         // console.log("Got user details from database");
+//         // console.log(`Username: ${this.username}`);
+//         console.log(this);
+        
+        
+        
+//     }
+// }
+
+// console.log(user.username);
+// console.log(user.getUserDetails);
+// console.log(this);
+
+
+
+// const promiseOne = new Promise()
+// const date = new Date()
+
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+        
+    }
+
+    return this
+}
+
+const userOne = new User("dilraj", 12, true)
+const userTwo = new User("SainoAurCode", 12, false)
+
+console.log(userOne.constructor);
+// console.log(userTwo);
+
+
+
+
 
 // node 15_classes_and_oop/oop.js
